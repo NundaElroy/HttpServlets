bbb8566169042816c76eb47467c5171678b8471e
NundaElroy <sekyezaelroy@gmail.com>
2025-01-07 23:42:28 +0300

    tomcat server added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,48 @@
 package org.example;
 
+import jakarta.servlet.Servlet;
+import org.apache.catalina.Context;
+
+import org.apache.catalina.startup.Tomcat;
+
+
+/*
+ * Context represents the  web app that's running on the server
+ * contextPath is the path that the app is available at
+ * doc base is the location of the web apps static files
+ * */
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws Exception {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        Context ctx = tomcat.addContext("", null);
+
+
+
+        // This is where we register your StudentServlet
+        // It's like @Controller in Spring, but manual
+
+
+         Tomcat.addServlet(ctx,"studentServlet", new StudentServlet());
+
+
+
+
+        // This maps URLs that start with /students/ to your servlet
+        // It's like @RequestMapping in Spring
+
+        ctx.addServletMappingDecoded("/students/*", "studentServlet");
+
+        // Start the server
+        tomcat.start();
+       // Keep it running (like how Spring Boot app stays running)
+        tomcat.getServer().await();
+
+
     }
 }
